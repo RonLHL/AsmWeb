9928235f0a1d9e289b6f040b0d7764ca47a72cbe
RonLHL <ronleehonglim2@gmail.com>
2024-05-21 11:37:35 +0800

    editProfile.php fucntion

--- memberProfile.php
+++ memberProfile.php
@@ -34,7 +34,7 @@ if (isset($_COOKIE["login-user"])) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>View History</title>
+        <title>Member Profile</title>
         <link href="css/style.css" rel="stylesheet" type="text/css">
         <link href="css/profile.css" rel="stylesheet" type="text/css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
